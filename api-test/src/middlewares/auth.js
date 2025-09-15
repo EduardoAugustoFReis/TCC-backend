b89dd32d8c08e362659b00a02218a88116d6de19
Lucas Oliveira <lucaseuof07@gmail.ocm>
2025-09-14 21:32:49 -0300

    adicionando logout no loginController, implementando a blacklist com redis e verificando a backlist no login

--- api-test/src/middlewares/auth.js
+++ api-test/src/middlewares/auth.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import {promisify}  from "util";
 import authConfig from "../config/authConfig.js";
+import redis from "../config/redis.js";
 
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -12,6 +13,12 @@ export default async (req, res, next) => {
   const [, token] = authHeader.split(" ");
 
   try {
+
+    // verifica se token está na blacklist
+    const isBlacklisted = await redis.get(`blacklist:${token}`);
+    if (isBlacklisted) {
+      return res.status(403).json({ error: "Token inválidado por logout" });
+    }
 
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
     
