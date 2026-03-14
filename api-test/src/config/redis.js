3a83c212d12a39106abf576067c20b3b5f9b5a87
Eduardo Reis <eduardo.afreis@gmail.com>
2026-03-14 16:41:02 -0300

    Config do Redis refatorada.

--- api-test/src/config/redis.js
+++ api-test/src/config/redis.js
@@ -1,8 +1,10 @@
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
-});
+const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL)
+  : new Redis({
+      host: "127.0.0.1",
+      port: 6379,
+    });
 
 export default redis;
